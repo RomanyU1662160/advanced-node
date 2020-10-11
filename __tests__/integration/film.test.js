7eda3a31c427c22ac126f47f5efea8812eecc6d3
Romany <u1662160@unimail.hud.ac.uk>
2020-10-11 16:02:55 +0100

    add integration tests for films

--- __tests__/integration/film.test.js
+++ __tests__/integration/film.test.js
@@ -16,4 +16,16 @@ describe("get api/films", () => {
     const res = await request(server).get("/api/films");
     expect(res.status).toBe(200);
   });
+  it("should populate director details ", async () => {
+    const director = {
+      name: "Director4",
+      email: "test2@test.com",
+    };
+    const res = await request(server).get("/api/films");
+    expect(res.body[0]["director"]).toMatchObject(
+      expect.objectContaining(director)
+    );
+    console.log("req.body ::>>>", res.body[0]);
+    expect(res.body[0]).toMatchObject({ name: "Film1", director });
+  });
 }, 50000);
--- __tests__/integration/middleware/authMiddleware.test.js
+++ __tests__/integration/middleware/authMiddleware.test.js
@@ -14,4 +14,21 @@ describe(" authMiddleware tests ", () => {
     const res = await request(server).post("/api/genres/");
     expect(res.status).toBe(401);
   });
-});
+
+  it("return 400 error when token is not correct", async () => {
+    const token = "fakeToken";
+    const res = await request(server)
+      .post("/api/genres/")
+      .set("x-auth-token", token);
+    expect(res.status).toBe(400);
+  });
+  it("return 200 error when token is correct", async () => {
+    const token = new User().generateAuthToken();
+    console.log("token in auth  ::>>>", token);
+    const res = await request(server)
+      .post("/api/genres")
+      .set("x-auth-token", token)
+      .send({ name: "Fake name" });
+    expect(res.status).toBe(200);
+  });
+}, 50000);
