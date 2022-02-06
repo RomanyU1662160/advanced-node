884d886e402f81e0a57204102c84a5e8826c252e
romany <u1662160@unimail.hud.ac.uk>
2022-02-06 18:02:41 +0000

    feat: add options toJson on userSchema

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -2,51 +2,60 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-const UserSchema = mongoose.Schema({
-  fname: {
-    type: String,
-    trim: true
-    // minLength: 3,
-    // maxLength: 50,
-    // required: true
+const UserSchema = mongoose.Schema(
+  {
+    fname: {
+      type: String,
+      trim: true,
+      // minLength: 3,
+      // maxLength: 50,
+      // required: true
+    },
+    lname: {
+      type: String,
+      trim: true,
+      minLength: 3,
+      maxLength: 50,
+      required: true,
+    },
+    email: {
+      type: String,
+      // unique: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 150,
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      minLength: 3,
+      maxLength: 1024,
+      required: true,
+    },
+    isGold: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  lname: {
-    type: String,
-    trim: true,
-    minLength: 3,
-    maxLength: 50,
-    required: true
-  },
-  email: {
-    type: String,
-    // unique: true,
-    trim: true,
-    minLength: 3,
-    maxLength: 150,
-    required: true
-  },
-  password: {
-    type: String,
-    trim: true,
-    minLength: 3,
-    maxLength: 1024,
-    required: true
-  },
-  isGold: {
-    type: Boolean,
-    default: false,
-    required: false
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.__v;
+      },
+    },
   }
-});
+);
 
 UserSchema.methods.generateAuthToken = function (expiryTime = 360000) {
   const payload = {
     id: this.id,
-    isAdmin: this.isAdmin
+    isAdmin: this.isAdmin,
   };
   const secret = config.get("JWT_SECRET");
 
